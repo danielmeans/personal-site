6e19b519482c1c4423318be18029da47f5d64bbd
Daniel Means <dmeans@cloudflare.com>
2023-02-07 16:33:16 -0800

    Update index desription

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,23 +5,20 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Daniel Means's personal website. Santa Barbara based Systems Engineer "}
+    description={"Daniel Means's personal website. Santa Barbara based Software Engineer "}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <h2 data-testid="heading"><Link to="/">welcome</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            I&apos;m a Santa Barbara based Software engineer working
+            on Machine Learning and Security problems.
           </p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
+      <p> Welcome to my website. Please feel free to check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
       <p> Source available <a href="https://github.com/danielmeans/personal-site">here</a>.</p>
